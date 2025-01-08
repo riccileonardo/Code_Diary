9ebf4f650b3490d81188c0bdc2765d873844fa08
riccileonardo <l.lopesricci@gmail.com>
2025-01-08 16:19:09 -0400

    Adiciona LandingPage.jsx

---
+++ LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Landing Page!</h1>
+      <p>Esta é uma página simples criada com React.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
